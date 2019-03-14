60ba9a4b00d681a4f62c2eeccc98c962be6aeec1
imjoseangel <josea.munoz@gmail.com>
2019-03-14 23:44:46 +0100

    Adding Day 3

---
+++ python/volcanoes-in-the-United-States/choropleth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Building Web Maps using Folium"""
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+import os
+import json
+import folium
+import pandas as pd
+
+# Get File Directory
+WORK_DIR = os.path.dirname((os.path.realpath(__file__)))
+
+# Loading the json data as python dictionary
+GEO_DATA = json.load(open(WORK_DIR + "/data/us-states.json"))
+EMP_DATA = pd.read_csv(WORK_DIR + "/data/us-unemployment.csv")
+
+#Create base map
+MAP = folium.Map(location=[37.0902, -100.7129], zoom_start=4)
+
+#Method to create Choropleth map, All parameters are mandatory
+folium.Choropleth(
+    geo_data=GEO_DATA,
+    data=EMP_DATA,
+    name='Unemployment Rate',
+    columns=['State', 'Unemployment'],
+    key_on='feature.id',
+    fill_color='YlGn',
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name='Unemployment Rate (%)').add_to(MAP)
+
+#Save the map
+MAP.save("choropleth.html")
---
+++ python/volcanoes-in-the-United-States/map.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Building Web Maps using Folium"""
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+import os
+import folium
+from folium.plugins import MarkerCluster
+import pandas as pd
+
+# Get File Directory
+WORK_DIR = os.path.dirname((os.path.realpath(__file__)))
+
+# Loading the json data as python dictionary
+DATA = pd.read_csv(WORK_DIR + "/data/Volcanoes_USA.txt")
+LAT = DATA['LAT']
+LON = DATA['LON']
+ELEVATION = DATA['ELEV']
+
+
+#Function to change colors
+def color_change(elev):
+    """Change Color depending on Mountain Elevation"""
+
+    if elev < 1000:
+        return 'green'
+    elif 1000 <= elev < 3000:
+        return 'orange'
+    else:
+        return 'red'
+
+
+#Create base map
+MAP = folium.Map(
+    location=[37.296933, -121.9574983],
+    zoom_start=5,
+    tiles="CartoDB dark_matter")
+
+#Create Cluster
+MARKER_CLUSTER = MarkerCluster().add_to(MAP)
+
+#Plot Markers
+for LAT, LON, ELEVATION in zip(LAT, LON, ELEVATION):
+    folium.CircleMarker(
+        location=[LAT, LON],
+        radius=9,
+        popup=str(ELEVATION) + " m",
+        fill_color=color_change(ELEVATION),
+        color="gray",
+        fill_opacity=0.9).add_to(MARKER_CLUSTER)
+
+#Save the map
+MAP.save("map.html")
