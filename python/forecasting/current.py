0f17adf913ec934a52f777bbbe0d7833fa90ad55
Jose Angel Munoz <josea.munoz@gmail.com>
2019-03-24 20:08:55 +0100

    Feature/day 11 (#16)
    * Adding Current Plots
    
    * Adding Forecast
    
    * Adding Index

---
+++ python/forecasting/current.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Forecasting in Python with Prophet"""
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import boxcox
+
+# Get File Directory
+WORK_DIR = os.path.dirname((os.path.realpath(__file__)))
+
+# Loading the json data as python dictionary
+
+DATA = pd.read_csv(WORK_DIR + "/data/daily_orders.csv")
+DATA.date = pd.to_datetime(DATA['date'], format='%Y-%m-%d %H:%M:%S')
+DATA['boxcox'], lam = boxcox(DATA['value'])
+
+# inplace is mandatory here. Have to assign back to dataframe
+# (because it is a new copy)
+
+DATA.set_index('date', inplace=True)
+
+# Print DATA
+DATA.plot(subplots=True, layout=(2, 1), figsize=(9, 9))
+plt.show()
