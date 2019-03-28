3d16d15245eedea4708ee53dc0f466b3c60071f8
imjoseangel <josea.munoz@gmail.com>
2019-03-28 21:22:25 +0100

    Adding speech Reco

---
+++ python/speech/cena.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Speech Recognition"""
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+print('Háztela Tú')
