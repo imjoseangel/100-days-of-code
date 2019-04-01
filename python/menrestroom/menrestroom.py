65dcd77389a32b9c2c7c8f65604c741fb2adb8c6
imjoseangel <josea.munoz@gmail.com>
2019-04-01 23:10:51 +0200

    Adding Threading

--- python/menrestroom/menrestroom.py
+++ python/menrestroom/menrestroom.py
@@ -7,9 +7,10 @@ from __future__ import (division, absolute_import, print_function,
 
 import random
 import time
-import os
+import threading
 
-TIMEPEEING = 1
+STALLFREQ = 1
+TIMEPEEING = 30
 STALLS = 10
 LIST_UNTAKEN = list(range(0, STALLS))
 LIST_TAKEN = []
@@ -21,12 +22,17 @@ STALL_PRINT = list(STR_EMPTY * STALLS)
 def take_stall():
     # Check if there is any empty stall
     if len(LIST_UNTAKEN) > 0:
-        new_stall = random.choice(LIST_UNTAKEN)
+        if not LIST_TAKEN:
+            new_stall = round(sum(LIST_UNTAKEN) / len(LIST_UNTAKEN) + .5)
+        else:
+            new_stall = random.choice(LIST_UNTAKEN)
         LIST_UNTAKEN.remove(new_stall)
         LIST_TAKEN.append(new_stall)
 
     STALL_PRINT.pop(LIST_TAKEN[-1])
     STALL_PRINT.insert(LIST_TAKEN[-1], STR_TAKEN)
+
+    threading.Timer(STALLFREQ, take_stall).start()
 
     return LIST_UNTAKEN, LIST_TAKEN, STALL_PRINT
 
@@ -40,27 +46,22 @@ def leave_stall():
         STALL_PRINT.pop(old_stall)
         STALL_PRINT.insert(old_stall, STR_EMPTY)
 
+    threading.Timer(TIMEPEEING, leave_stall).start()
+
     return LIST_UNTAKEN, LIST_TAKEN, STALL_PRINT
 
 
 def main():
 
-    peeing = TIMEPEEING
-    while len(LIST_UNTAKEN) > 0:
+    # peeing = threading.Timer(5.0, leave_stall)
+    take_stall()
+    leave_stall()
 
-        take_stall()
-        os.system('clear')
-        print('\n')
+    while len(LIST_UNTAKEN) > 0:
         for item in STALL_PRINT:
             print(item, end=' ', flush=True)
-        time.sleep(1)
 
-        if peeing == 0:
-            leave_stall()
-            peeing = TIMEPEEING
-        else:
-            peeing -= 1
-
+        time.sleep(STALLFREQ)
         print('\n')
 
 
