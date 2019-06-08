aee3aa819005fdfb43a3e4095af2a5ed2006fd08
imjoseangel <josea.munoz@gmail.com>
2019-06-08 10:31:04 +0200

    Adding Git Subdirs

--- python/gitsync/updaterepos.py
+++ python/gitsync/updaterepos.py
@@ -5,6 +5,7 @@ from __future__ import (division, absolute_import, print_function,
                         unicode_literals, annotations)
 import os
 import argparse
+import subprocess
 
 fallback_args = dict(basedir=os.path.dirname((os.path.realpath(__file__))))
 
@@ -14,9 +15,28 @@ class runupdate():
 
         # Parse arguments passed at cli
         self.parse_arguments()
+        self.update_git()
 
-        for dirname in next(os.walk(self.args.basedir))[1]:
-            print(dirname)
+    @staticmethod
+    def get_directories(directory):
+        return next(os.walk(directory))[1]
+
+    def update_git(self):
+        directories = self.get_directories(self.args.basedir)
+        for directory in directories:
+            dir_fullpath = self.args.basedir + '/' + directory
+            subdirs = self.get_directories(dir_fullpath)
+            if '.git' in subdirs:
+                proc = subprocess.Popen("git remote prune origin",
+                                        shell=True,
+                                        executable="/bin/bash",
+                                        cwd=dir_fullpath,
+                                        stdin=None,
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.STDOUT)
+                proc.wait()
+                for line in iter(proc.stdout.readline, b''):
+                    print(line)
 
     def parse_arguments(self):
         parser = argparse.ArgumentParser(description='Update Git Repositories')
