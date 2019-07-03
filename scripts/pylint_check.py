4fe906859ea745b035a9aee7bab354a7be204ba8
Jose Angel Munoz <josea.munoz@gmail.com>
2019-07-03 09:35:28 +0200

    Feature/day 58 (#61)
    * Adding new and improved script
    
    * Adding requirements
    
    * Adding importlib
    
    * Adding importlib
    
    * Adding Fetch
    
    * Adding Fetch
    
    * Removing all option
    
    * Adding Radon Check
    
    * Adding Compare with devel

--- scripts/pylint_check.py
+++ scripts/pylint_check.py
@@ -4,80 +4,98 @@
 
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
+import importlib
 import os
 import sys
 import re
 import subprocess
-from subprocess import STDOUT, PIPE
+
+try:
+    importlib.import_module('git')
+    import git
+except ImportError:
+    print("GitPython is not installed")
+    sys.exit(1)
 
 
-class WorkPath():
-    """WorkPath Class"""
+class WorkPath:
+    # WorkPath Class
 
-    def __init__(self):
+    def __init__(self, branch):
         self._path = os.path.dirname(os.path.realpath(__file__))
         self._work_path = os.path.dirname(self._path)
         os.chdir(self._work_path)
+        self.branch = branch
 
     def workpath(self):
-        """Return Work Path"""
+        # Return Work Path
         return self._work_path
 
     def path(self):
-        """Return Path"""
+        # Return Path
         return self._path
+
+    def __gitdownload(self):
+
+        self.workpath()
+        repo = git.Repo()
+        repo.git.remote('set-branches', 'origin', self.branch)
+        for remote in repo.remotes:
+            remote.fetch()
+        for item in repo.index.diff('origin/{0}'.format(self.branch)):
+            yield item.a_path
+
+    def getchanges(self):
+
+        changes = list(self.__gitdownload())
+        return changes
 
 
 def asterisks():
-    """Calculate Screen Size"""
-
+    # Calculate Screen Size
     rows, columns = os.popen('stty size', 'r').read().split()
-
     if rows:
         pass
-
     print('-' * int(columns))
 
 
 def main():
-    """Main Function"""
+    # Main Function
 
     # Sets default exit status code
     return_code = 0
 
-    myproject = WorkPath()
+    myproject = WorkPath('devel')
+    project_changes = myproject.getchanges()
     work_path = myproject.workpath()
 
-    for dirpath, dirnames, filenames in os.walk(work_path):
-        if dirnames:
-            pass
-        for filename in filenames:
-            if filename.endswith('.py'):
-                pythonfile = os.path.join(dirpath, filename)
+    for filename in project_changes:
+        if filename.endswith('.py'):
+            pythonfile = os.path.join(work_path, filename)
 
-                print("Filename: %s" % pythonfile)
-                asterisks()
+            print("Filename: {0}".format(pythonfile))
+            asterisks()
 
-                proc = subprocess.Popen('pylint %s' % pythonfile,
-                                        shell=True,
-                                        executable="/bin/bash",
-                                        stdin=None,
-                                        stdout=PIPE,
-                                        stderr=STDOUT)
-                proc.wait()
+            proc = subprocess.Popen('pylint %s' % pythonfile,
+                                    shell=True,
+                                    executable="/bin/bash",
+                                    stdin=None,
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.STDOUT)
+            proc.wait()
 
-                for line in iter(proc.stdout.readline, b''):
+            for line in iter(proc.stdout.readline, b''):
 
-                    score = re.search(r"(?P<score>\d?\d\.\d\d)",
-                                      line.decode('UTF-8'))
-                    if score:
-                        print(line.decode('UTF-8'))
-                        rate = float(score.group('score'))
-                    else:
-                        rate = float(10.0)
+                score = re.search(r"(?P<score>\d?\d\.\d\d)",
+                                  line.decode('UTF-8'))
+                if score:
+                    print(line.decode('UTF-8'))
+                    rate = float(score.group('score'))
+                else:
+                    rate = float(10.0)
 
-                    if rate < 9.0:
-                        return_code = 1
+                if rate < 9.0:
+                    return_code = 1
 
     sys.exit(return_code)
 
--- scripts/radon_check.py
+++ scripts/radon_check.py
@@ -4,75 +4,93 @@
 
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
+import importlib
 import os
 import sys
 import subprocess
-from subprocess import STDOUT, PIPE
+
+try:
+    importlib.import_module('git')
+    import git
+except ImportError:
+    print("GitPython is not installed")
+    sys.exit(1)
 
 
-class WorkPath():
-    """WorkPath Class"""
+class WorkPath:
+    # WorkPath Class
 
-    def __init__(self):
+    def __init__(self, branch):
         self._path = os.path.dirname(os.path.realpath(__file__))
         self._work_path = os.path.dirname(self._path)
         os.chdir(self._work_path)
+        self.branch = branch
 
     def workpath(self):
-        """Return Work Path"""
+        # Return Work Path
         return self._work_path
 
     def path(self):
-        """Return Path"""
+        # Return Path
         return self._path
+
+    def __gitdownload(self):
+
+        self.workpath()
+        repo = git.Repo()
+        repo.git.remote('set-branches', 'origin', self.branch)
+        for remote in repo.remotes:
+            remote.fetch()
+        for item in repo.index.diff('origin/{0}'.format(self.branch)):
+            yield item.a_path
+
+    def getchanges(self):
+
+        changes = list(self.__gitdownload())
+        return changes
 
 
 def asterisks():
-    """Calculate Screen Size"""
-
+    # Calculate Screen Size
     rows, columns = os.popen('stty size', 'r').read().split()
-
     if rows:
         pass
-
     print('-' * int(columns))
 
 
 def main():
-    """Main Function"""
+    # Main Function
 
     # Sets default exit status code
     return_code = 0
 
-    myproject = WorkPath()
+    myproject = WorkPath('devel')
+    project_changes = myproject.getchanges()
     work_path = myproject.workpath()
 
-    for dirpath, dirnames, filenames in os.walk(work_path):
-        if dirnames:
-            pass
-        for filename in filenames:
-            if filename.endswith('.py'):
-                pythonfile = os.path.join(dirpath, filename)
-                try:
-                    proc = subprocess.Popen('radon cc %s' % pythonfile,
-                                            shell=True,
-                                            executable="/bin/bash",
-                                            stdin=None,
-                                            stdout=PIPE,
-                                            stderr=STDOUT)
-                    proc.wait()
+    for filename in project_changes:
+        if filename.endswith('.py'):
+            pythonfile = os.path.join(work_path, filename)
+            try:
+                proc = subprocess.Popen('radon cc %s' % pythonfile,
+                                        shell=True,
+                                        executable="/bin/bash",
+                                        stdin=None,
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.STDOUT)
+                proc.wait()
 
-                    asterisks()
+                asterisks()
 
-                    for line in iter(proc.stdout.readline, b''):
-                        print(line.decode('UTF-8'))
+                for line in iter(proc.stdout.readline, b''):
+                    print(line.decode('UTF-8'))
 
-                    if proc.returncode > 0:
-                        return_code = 1
-
-                except subprocess.CalledProcessError as exception:
-                    print(exception.output)
+                if proc.returncode > 0:
                     return_code = 1
+
+            except subprocess.CalledProcessError as exception:
+                print(exception.output)
+                return_code = 1
 
     sys.exit(return_code)
 
