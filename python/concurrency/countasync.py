e3450a37b6ec15c4903fc6c41bb0eed358132f34
imjoseangel <josea.munoz@gmail.com>
2019-07-11 22:54:38 +0200

    Adding Sync and Async

---
+++ python/concurrency/countasync.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+import asyncio
+
+
+async def count():
+    print("One")
+    await asyncio.sleep(1)
+    print("Two")
+
+
+async def main():
+    await asyncio.gather(count(), count(), count())
+
+
+if __name__ == '__main__':
+    import time
+    s = time.perf_counter()
+    asyncio.run(main())
+    elapsed = time.perf_counter() - s
+    print(f"{__file__} executed in {elapsed:0.2f} seconds.")
