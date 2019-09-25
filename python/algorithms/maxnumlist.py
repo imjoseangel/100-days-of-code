eae6a57b0ca6092bc621f0f186a850efa77fd2cc
imjoseangel <josea.munoz@gmail.com>
2019-09-25 10:02:37 +0200

    Fixing Array

--- python/algorithms/maxnumlist.py
+++ python/algorithms/maxnumlist.py
@@ -5,18 +5,16 @@ from __future__ import (division, absolute_import, print_function)
 
 
 def maxlist(array):
-    if not array:
-        return 0
-    elif len(array) == 1:
-        return array[0]
-    elif len(array) == 2:
+    if len(array) < 2:
+        return array if not array else array[0]
+    else:
         return array[0] if array[0] > array[1] else array[1]
     sub_max = maxlist(array[1:])
     return array[0] if array[0] > sub_max else sub_max
 
 
 def main():
-    myarray = maxlist([1, 40, 5, 2])
+    myarray = maxlist([1])
     print(myarray)
 
 
