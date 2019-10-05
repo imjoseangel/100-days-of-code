96ce84634bfe51c53df35961b0dd1d8999297d7e
Jose Angel Munoz <josea.munoz@gmail.com>
2019-10-05 13:23:38 +0200

    Add greedy alg (#74)

---
+++ python/grokalgorithms/greedy.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function)
+
+
+def main():
+
+    states_needed = set(["mt", "wa", "or", "id", "nv", "ut", "ca", "az"])
+
+    stations = {}
+    stations["kone"] = set(["id", "nv", "ut"])
+    stations["ktwo"] = set(["id", "wa", "mt"])
+    stations["kthree"] = set(["or", "nv", "ca"])
+    stations["kfour"] = set(["nv", "ut"])
+    stations["kfive"] = set(["ca", "az"])
+
+    final_stations = set()
+
+    while states_needed:
+
+        best_station = None
+        states_covered = set()
+
+        for station, states_for_station in stations.items():
+            covered = states_needed & states_for_station
+            if len(covered) > len(states_covered):
+                best_station = station
+                states_covered = covered
+
+        states_needed -= states_covered
+        final_stations.add(best_station)
+
+        print(final_stations)
+
+
+if __name__ == '__main__':
+    main()
