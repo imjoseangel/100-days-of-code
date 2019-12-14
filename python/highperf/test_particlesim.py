614afd4ab7455b893ffad6aaae1574c17ad2cca5
imjoseangel <josea.munoz@gmail.com>
2019-12-14 13:14:06 +0100

    Fix Lint

---
+++ python/highperf/test_particlesim.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import (division, absolute_import, print_function,
+                        unicode_literals)
+
+from particlesim import Particle, ParticleSimulator
+
+
+def test_evolve(benchmark):
+    particles = [
+        Particle(0.3, 0.5, +1),
+        Particle(0.0, -0.5, -1),
+        Particle(-0.1, -0.4, +3)
+    ]
+
+    simulator = ParticleSimulator(particles)
+
+    simulator.evolve(0.1)
+
+    p0, p1, p2 = particles
+
+    def fequal(a, b, eps=1e-5):
+        return abs(a - b) < eps
+
+    assert fequal(p0.x, 0.210269)
+    assert fequal(p0.y, 0.543863)
+
+    assert fequal(p1.x, -0.099334)
+    assert fequal(p1.y, -0.490034)
+
+    assert fequal(p2.x, 0.191358)
+    assert fequal(p2.y, -0.365227)
+
+    benchmark(simulator.evolve, 0.1)
