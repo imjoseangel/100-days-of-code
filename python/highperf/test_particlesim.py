5c7a660e4eaa10aa249681ff77e28f6ab9f3253e
imjoseangel <josea.munoz@gmail.com>
2020-01-03 21:33:52 +0100

    Add memmemory benchmarks

--- python/highperf/test_particlesim.py
+++ python/highperf/test_particlesim.py
@@ -33,3 +33,31 @@ def test_evolve(benchmark):
     assert fequal(p2.y, -0.365227)
 
     benchmark(simulator.evolve, 0.1)
+
+
+def test_evolve_fast(benchmark):
+    particles = [
+        Particle(0.3, 0.5, +1),
+        Particle(0.0, -0.5, -1),
+        Particle(-0.1, -0.4, +3)
+    ]
+
+    simulator = ParticleSimulator(particles)
+
+    simulator.evolve_fast(0.1)
+
+    p0, p1, p2 = particles
+
+    def fequal(a, b, eps=1e-5):
+        return abs(a - b) < eps
+
+    assert fequal(p0.x, 0.210269)
+    assert fequal(p0.y, 0.543863)
+
+    assert fequal(p1.x, -0.099334)
+    assert fequal(p1.y, -0.490034)
+
+    assert fequal(p2.x, 0.191358)
+    assert fequal(p2.y, -0.365227)
+
+    benchmark(simulator.evolve, 0.1)
