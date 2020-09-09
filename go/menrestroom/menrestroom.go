619d40539cd1a4b6964f3d501f9211f6cb67ecad
imjoseangel <josea.munoz@gmail.com>
2020-09-09 15:03:33 +0200

    Basic Vars

--- go/menrestroom/menrestroom.go
+++ go/menrestroom/menrestroom.go
@@ -1,10 +1,22 @@
 ///bin/true; exec /usr/bin/env go run "" "$@"
 package main
 
+import (
+	"fmt"
+	"math"
+	"math/rand"
+	"time"
+)
+
 var stalls int = 10
 var stallfreq int = 2
 var mintimepeeing int = 1
 var maxtimepeeing int = 10
+
+const emoEmpty string = "\U0001F6BD"
+const emoTaken string = "\U0001F6B6"
+const emoDoor string = "\U0001F6AA"
+const halfValue float32 = 0.5
 
 func makeRange(min, max int) []int {
 	a := make([]int, max-min+1)
@@ -14,12 +26,25 @@ func makeRange(min, max int) []int {
 	return a
 }
 
+func sum(array []int) int {
+	result := 0
+	for _, value := range array {
+		result += value
+	}
+	return result
+}
+
 func takeStall() {
 
+	rand.Seed(time.Now().UnixNano())
+	timepeeing := rand.Intn(maxtimepeeing-mintimepeeing+1) + mintimepeeing
 	untaken := makeRange(1, stalls)
 	taken := make([]int, stalls)
+	newStall := math.Floor(float64(sum(untaken)) / float64(len(untaken)))
+	fmt.Println(emoDoor, emoEmpty, emoTaken, timepeeing, taken, newStall)
 }
 
 func main() {
 
+	takeStall()
 }
