ab09999e082dd67ec603d36866b5b4eceb014ac3
imjoseangel <josea.munoz@gmail.com>
2021-02-04 14:59:17 +0100

    Add by date

--- python/syslogstats/syslogstats.py
+++ python/syslogstats/syslogstats.py
@@ -11,13 +11,14 @@ def main():
     syslogf = open('syslog', 'r')
 
     regex = re.compile(
-        r"^(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s{1,2}\d{1,2}")
+        r"^((Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s{1,2}\d{1,2})"
+        r"\s\d{2}:\d{2}:\d{2}\s\S{1,256}\s(\S{1,256})\W\d{1,256}\W")
 
     lines = syslogf.readlines()
     for line in lines:
         result = regex.search(line)
         if result:
-            print(result.group(0))
+            print(result.group(1) + " " + result.group(3))
 
 
 if __name__ == '__main__':
