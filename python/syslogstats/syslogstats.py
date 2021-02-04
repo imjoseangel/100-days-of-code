2ba7488567abed9d6d227b7eb0460706dba561cd
imjoseangel <josea.munoz@gmail.com>
2021-02-04 14:51:21 +0100

    Add result

--- python/syslogstats/syslogstats.py
+++ python/syslogstats/syslogstats.py
@@ -4,12 +4,20 @@
 from __future__ import (division, absolute_import, print_function,
                         unicode_literals)
 
+import re
+
 
 def main():
     syslogf = open('syslog', 'r')
 
+    regex = re.compile(
+        r"^(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)\s{1,2}\d{1,2}")
+
     lines = syslogf.readlines()
-    print(lines)
+    for line in lines:
+        result = regex.search(line)
+        if result:
+            print(result.group(0))
 
 
 if __name__ == '__main__':
