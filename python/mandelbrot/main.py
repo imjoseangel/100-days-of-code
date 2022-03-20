0e1c2d34ad2256a31b878b598d4ce5005157bd8c
imjoseangel <josea.munoz@gmail.com>
2022-03-20 13:08:05 +0100

    fix pylint

--- python/mandelbrot/main.py
+++ python/mandelbrot/main.py
@@ -1,9 +1,23 @@
-def z(n, c):
-    if n == 0:
+def mandelbrot(number, c):
+    if number == 0:
         return 0
 
-    return z(n - 1, c) ** 2 + c
+    return mandelbrot(number - 1, c) ** 2 + c
+
+
+def sequence(c):
+    seq = 0
+    while True:
+        yield seq
+        seq = seq ** 2 + c
 
 
 for n in range(10):
-    print(f"z({n}) = {z(n, c=1)}")
+    print(f"mandelbrot({n}) = {mandelbrot(n, c=1)}")
+
+
+# More efficient way to calculate the sequence
+for n, z in enumerate(sequence(c=1)):
+    print(f"mandelbrot({n}) = {z}")
+    if n >= 9:
+        break
