74fb88f471c8df7b83a240702485f6788be8fde1
imjoseangel <josea.munoz@gmail.com>
2022-03-20 13:12:32 +0100

    add julia

--- python/mandelbrot/main.py
+++ python/mandelbrot/main.py
@@ -1,22 +1,17 @@
-def mandelbrot(number, c):
-    if number == 0:
-        return 0
-
-    return mandelbrot(number - 1, c) ** 2 + c
-
-
-def sequence(c):
-    seq = 0
+def sequence(c, seq=0):
     while True:
         yield seq
         seq = seq ** 2 + c
 
 
-for n in range(10):
-    print(f"mandelbrot({n}) = {mandelbrot(n, c=1)}")
+def mandelbrot(candidate):
+    return sequence(seq=0, c=candidate)
 
 
-# More efficient way to calculate the sequence
+def julia(candidate, parameter):
+    return sequence(seq=candidate, c=parameter)
+
+
 for n, z in enumerate(sequence(c=1)):
     print(f"mandelbrot({n}) = {z}")
     if n >= 9:
